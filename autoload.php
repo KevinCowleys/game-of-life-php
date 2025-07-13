b556705a9439210badb1752eaf2c69fd30bf25fd
KevinCowleys <KevinCowleys@gmail.com>
2025-07-13 19:35:21 +0200

    Updated autoloader to work with tests namespaces

--- autoload.php
+++ autoload.php
@@ -1,16 +1,30 @@
 <?php
 
 spl_autoload_register(function ($className) {
-    // Changing the slashes for Linux
-    if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
-        $className = str_replace('\\', '/', $className);
+    if (!is_string($className)) return false;
+
+    // Map the root namespace to folders
+    $namespaceMap = [
+        'GameOfLife' => __DIR__ . '/src/GameOfLife',
+        'Tests'      => __DIR__ . '/tests',
+    ];
+
+    foreach ($namespaceMap as $namespace => $baseDir) {
+        if (strpos($className, $namespace . '\\') === 0) {
+            // Normalize the namespace separators for any OS
+            $file = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+
+            // Replace the namespace prefix with its folder
+            $relative = substr($file, strlen($namespace));
+            $path = $baseDir . $relative;
+
+            if (file_exists($path)) {
+                require_once $path;
+                return true;
+            }
+        }
     }
 
-    $file = __DIR__ . '/src/' . $className . '.php';
-
-    if (file_exists($file)) {
-        require_once $file;
-    } else {
-        echo "Class file for '$className' not found.";
-    }
+    echo "Class file for '$className' not found.";
+    return false;
 });
