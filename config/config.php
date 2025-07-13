96e7e6e86be422767c2db7412bc7a802d8c787a0
KevinCowleys <KevinCowleys@gmail.com>
2025-07-13 17:39:06 +0200

    Added defaults and patterns

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'infinite' => false,
+    'speed' => 500,
+    'Width' => 25,
+    'Height' => 25,
+    'pattern' => 0,
+    'symbolAlive' => '🟩',
+    'symbolDead' => '⬛️',
+];
